8cd33bb3028545252d82b5849bb8bcaf9835c84b
Abd Asis <abdasis@outlook.com>
2020-05-18 16:54:33 +0700

    first commit

---
+++ resources/views/frontend/includes/navbar.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg fixed-top navbar-custom sticky sticky-dark">
+    <div class="container-fluid">
+        <!-- LOGO -->
+        <a class="logo text-uppercase" href="index.html">
+            <img src="{{ url('/') }}/frontend/assets/images/logo-light.png" alt="" class="logo-light" height="21" />
+            <img src="{{ url('/') }}/frontend/assets/images/logo-dark.png" alt="" class="logo-dark" height="21" />
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="mdi mdi-menu"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ml-auto navbar-center" id="mySidenav">
+                <li class="nav-item active">
+                    <a href="#home" class="nav-link">Product</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#features" class="nav-link">Produk</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#demo" class="nav-link">Proyek</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#pricing" class="nav-link">Berita</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#faq" class="nav-link">Tentang Kami</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#clients" class="nav-link">Hubungan Kami</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
